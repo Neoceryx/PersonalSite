7c8ccc70db333016401d5b39e1cca5896635e29c
Neoceryx <dani.fierro796@gmail.com>
2020-07-04 22:57:12 -0700

    Personal View has been started

--- DanApp/infoapp/views.py
+++ DanApp/infoapp/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello django")
-    pass
-
 def index(request):
     return render(request,"views/index.html",{})
     pass
