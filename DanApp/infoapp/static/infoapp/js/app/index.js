4a29598c2d21237f9b1205d833551663778220b8
daniel.fierro <daniel.fierro@mitechnologiesinc.com>
2020-07-08 15:15:14 -0700

    Angularjs Was integrated

---
+++ DanApp/infoapp/static/infoapp/js/app/index.js
@@ -0,0 +1,13 @@
+var app = angular.module("App", []);
+
+app.controller("AppCTRL", function ($scope) {
+
+    // Document on ready
+    angular.element(document).ready(function () {
+        console.log("Hi");
+    })
+    // Document on ready
+
+
+    
+});
