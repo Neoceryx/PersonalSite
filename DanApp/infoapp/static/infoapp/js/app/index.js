4c0ca9da2b53dd8d8951309bef7771b7a98a95ab
Neoceryx <dani.fierro796@gmail.com>
2020-07-11 23:37:37 -0700

    Spanish Informaiton hd been started

--- DanApp/infoapp/static/infoapp/js/app/index.js
+++ DanApp/infoapp/static/infoapp/js/app/index.js
@@ -4,13 +4,13 @@ app.config(function ($routeProvider,$translateProvider) {
     
     // English
     $translateProvider.translations("en",{
-        TEST:"I am a software developer with 3 years of experience, throughout my career I have been able to help solve different problems by creating digital tools to facilitate work processes and saving costs, mainly focused on the manufacturing sector. through the areas of Production lines, Warehouses, Human Resources, Sales "
+        ABOUT:"I am a software developer with 3 years of experience, throughout my career I have been able to help solve different problems by creating digital tools to facilitate work processes and saving costs, mainly focused on the manufacturing sector. through the areas of Production lines, Warehouses, Human Resources, Sales "
     });
     // End English
 
     // Spanish
     $translateProvider.translations("es",{
-        TEST:"En español"
+        ABOUT:"Soy un desarrollador de software con 3 años de experiencia, a lo largo de mi carrera, he podido ayudar a resolver diferentes problemas principalmente enfocado al sector de manufactura, mediante la creación de herramientas digitales, para facilitar procesos de trabajo y ahorro de costos. Atreves de las áreas de Líneas de producción, Almacenes, RH y área de ventas"
     });
     // End Spanish
 
@@ -34,9 +34,13 @@ app.controller("AppCTRL", function ($scope, $translate) {
         PHP: 70,
         HTML:90,
         CSS:75,
+        JQUERY: 90,
         ANGULARJS:80,
         VUEJS:60,
-        REACT:50,        
+        REACT:50, 
+        REACTNATIVE:50,  
+        PYTHON:50,  
+        AMWS: 80   
     }
     
     $scope.ChangeLanguage = function (languaje) {        
